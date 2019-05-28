455327211df1f74fc2658d1cdde58b9ced4a99a8
Jiahao Chen <1732+jiahao@users.noreply.github.com>
2019-05-28 17:24:41 -0400

    gf.jl: clean up
    Signed-off-by: Jiahao Chen <1732+jiahao@users.noreply.github.com>

--- src/gf.jl
+++ src/gf.jl
@@ -1,5 +1,7 @@
 """
 Galois fields
+
+Currently only supports p^n finite fields over the integers 0:p-1 where n=1 and p is prime. Does not support any fancy divisors.
 """
 module GaloisFields
 
@@ -9,6 +11,7 @@ import Base: ==, +, -, *, /, \, oneunit, inv, promote_rule, rand, convert
 
 abstract type SecretDomain <: Number end
 abstract type FiniteField <: SecretDomain end
+
 
 struct GF{T,q} <: FiniteField
     val :: T
@@ -31,20 +34,15 @@ function inv(a::GF{T,q}) where {T,q}
     x, b, _ = gcdx(a.val, q)
     if x == 1
         return GF{T, q}(mod(b, q))
-    else
+    else #Multiplicative inverse does not exist
         throw(DivideError)
     end
 end
 
-#XXX Missing promote_rule in base?
-promote_rule(::Type{<:Integer}, ::Type{BigInt}) = BigInt
-
-#Promote any binary operation with some integer into the field
-promote_rule(x::Type{GF{T,q}}, ::Type{S}) where {q, S, T} = promote_rule(x, GF{S,q})
+#Promote any binary operation with some other number into the field
 promote_rule(::Type{S}, x::Type{GF{T,q}}) where {q, S, T} = promote_rule(GF{S,q}, x)
-promote_rule(::Type{GF{S,q}}, ::Type{GF{T,q}}) where {q, S, T} = GF{promote_rule(S, T), q}
-promote_rule(::Type{GF{T,q}}, ::Type{GF{T,q}}) where {q, T} = GF{T, q}
-
+#Use widest internal representation
+promote_rule(::Type{GF{S,q}}, ::Type{GF{T,q}}) where {q, S, T} = GF{promote_type(S, T), q}
 
 Random.rand(rng::AbstractRNG, ::Random.SamplerType{GF{T,q}}) where {T,q} =
     GF(T, q, rand(rng, 0:q-1))
